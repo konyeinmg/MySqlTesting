0f8014df36526285ceff016173ffe851b5ce62b5
Nyein Chan Aung <nyeinchanaung.myeik@gmail.com>
2019-01-31 08:45:57 +0630

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+
+namespace mysqltesting
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            string name = textBoxName.Text;
+            string password = passwordBox.Password;
+            string path = "server=localhost;uid=root;pwd=root;database=student";
+
+            MySqlConnection con = new MySqlConnection(path);
+
+            try
+            {
+                con.Open();
+
+                MySqlCommand cmd = new MySqlCommand("select * from admin where name='" + name + "' and password='"+password+"'", con);
+                MySqlDataReader data = cmd.ExecuteReader();
+
+                if (data.Read())
+                {
+                    MessageBox.Show("Login is corrrect");
+                    this.Hide();
+                    new Window1().Show();
+                }
+                else MessageBox.Show("Invalid Login");
+
+            }catch(Exception ex)
+            {
+                con.Close();
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+}
---
+++ Page1.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+namespace mysqltesting
+{
+    /// <summary>
+    /// Interaction logic for Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void btninsert(object sender, RoutedEventArgs e)
+        {
+            string path = "server=localhost;uid=root;pwd=root;database=sale";
+            MySqlConnection con = new MySqlConnection(path);
+            con.Open();
+            string name = textBoxName.Text;
+            string roll = textBoxroll.Text;
+            string email = textBoxemail.Text;
+            string phone = textBoxphone.Text;
+            MySqlCommand cmd = new MySqlCommand("insert into studentInfo(name,rollno,email,phone) values (" + name + "," + roll + "," + email + "," + phone + ")", con);
+            int result = cmd.ExecuteNonQuery();
+            if(result > 0)
+            {
+                MessageBox.Show("Ok");
+            }
+            con.Close();
+        }
+    }
+}
